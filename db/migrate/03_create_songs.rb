121934d173d02af18830947acf8d8494af766338
Marcus Plenty <marcusplenty@gmail.com>
2018-12-30 02:22:02 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.artist_id :integer
+      t.genre_id :integer
+    end
+  end
 end
